441b8141148602f6fa1043eec6c4e793a8f12402
Nas-hash-source <79455907+Nas-hash-source@users.noreply.github.com>
2022-07-22 00:06:41 +0300

    Update temple.js

--- lesson12/js/temple.js
+++ lesson12/js/temple.js
@@ -1,5 +1,5 @@
 //function to create the temple card
-const requestURL='../data.json';
+const requestURL='https://github.com/Nas-hash-source/wdd230/lesson12/data.json';
 function templeCard() {
     fetch(requestURL).then(function (response) {
       return response.json();
